00d7209c14dc4414988d78b0b87e441f0575cf80
Sabrina Estévez <sesmo.dev@gmail.com>
2024-03-06 21:29:33 +0000

    Starting to add some design elements

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
+import SimpleLogo from "../../img/Logo-solo-tr.png"
+
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with 🎁 by{" "}
-			<a href="https://github.com/SabrinaESMO">Sabri</a>
-			<span className="name-separator"> - </span>
-			<a href="https://www.linkedin.com/in/mjuliafb/">MJ</a>
-		</p>
+	<footer className="footer sticky-bottom container-fluid">
+		<div className="nav-fill d-flex">
+			<p className="nav-item">
+			🎁 Made by{" "}
+				<a href="https://github.com/SabrinaESMO">Sesmo.dev</a>
+				<span className="name-separator"> & </span>
+				<a href="https://www.linkedin.com/in/mjuliafb/">MJ</a>
+			</p>
+			<img src={SimpleLogo} className="footer-logo">
+			</img>
+		</div>
 	</footer>
 );
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,31 +1,32 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import { Link, Navigate, useNavigate } from "react-router-dom";
+import Logo from "../../img/Logo-completo-tr.png"
+
 
 export const Navbar = () => {
 	const { store, actions } = useContext(Context);
 	const navigate = useNavigate()
 
-
 	const handleClick = () => {
 		actions.logout()
 		navigate('/')
-
 	}
 
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<span className="navbar-brand mb-0 h1">
+						<img src={Logo} className="Logo"/>
+					</span>	
 				</Link>
 				<div className="ml-auto">
 					{!store.token ? <Link to="/login">
-						<button className="btn btn-primary">Log in</button>
+						<button className="btn">Log in</button>
 					</Link> :
-						<button onClick={handleClick} className="btn btn-primary">Log out</button>
+						<button onClick={handleClick} className="btn">Log out</button>
 					}
-
 				</div>
 			</div>
 		</nav>
