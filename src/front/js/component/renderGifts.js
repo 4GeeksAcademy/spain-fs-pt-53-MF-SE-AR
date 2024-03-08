685196a45dd8732c348e704001aa2e12f4e948d2
Maria Julia Fernandez Balice (MJ) <136009647+Mjuliafb@users.noreply.github.com>
2024-03-08 19:35:25 +0000

    renderGift components, pending url from register and login , gifts flux

--- src/front/js/component/renderGifts.js
+++ src/front/js/component/renderGifts.js
@@ -10,10 +10,147 @@ export const RenderGifts = () => {
     const navigate = useNavigate()
     const lid = 1
 
-
     return (
-        <div>
-            <h1> Todos los regalos</h1>
+        <div className="col">
+            <div className="card">
+                <div className="top-icons-card d-flex justify-content-end p-2">
+                    <i className="fa-solid fa-circle-xmark"></i>
+                </div>
+                <div className="imgCard text-center">
+                </div>
+                <div className="card-body">
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item">
+                            <div className="input-group mb-3">
+                                <span className="input-group-text">
+                                    Título:
+                                </span>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    aria-label="Sizing example input"
+                                    disabled
+                                />
+                            </div>
+                        </li>
+                        <li className="list-group-item">
+                            <div className="input-group mb-3">
+                                <span className="input-group-text">
+                                    Link:
+                                </span>
+                                <input
+                                    type="text"
+                                    className="form-control custom-link"
+                                    aria-label="Sizing example input"
+                                    readOnly
+                                />
+                                {/* <a href={item.link} target="_blank" rel="noopener noreferrer">
+                                    <i className="fa-solid fa-globe"></i>
+                                </a> */}
+                            </div>
+                        </li>
+                        <li className="list-group-item">
+                            <div className="input-group mb-3">
+                                <label className="input-group-text">
+                                    Estatus:
+                                </label>
+                                <select
+                                    className="form-select"
+                                    disabled
+                                >
+                                    <option value=""></option>
+                                    <option value="Disponible">Disponible</option>
+                                    <option value="Reservado">Reservado</option>
+                                </select>
+                            </div>
+                        </li>
+                    </ul>
+                    <div className="card-footer text-center">
+                        <Link to="#">
+                            <button href="#" className="btn btn-primary">Editar</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
+
+// return store.gift.map((item, index) => (
+//     <div key={item.id} className="col">
+
+//         <div className="card">
+//             <div className="top-icons-card d-flex justify-content-end p-2">
+//                 <i className="fa-solid fa-circle-xmark" onClick={() => handleDelete(item.id)}></i>
+//             </div>
+//             <div className="imgCard text-center">
+//                 {randomImage && <img src={randomImage} className="card-img-top" alt="..." />}
+
+//             </div>
+//             <div className="card-body">
+//                 <ul className="list-group list-group-flush">
+//                     <li className="list-group-item">
+//                         <div className="input-group mb-3">
+//                             <span className="input-group-text" id={`title${item.id}`}>
+//                                 Título:
+//                             </span>
+//                             <input
+//                                 type="text"
+//                                 className="form-control"
+//                                 id={`title${item.id}`}
+//                                 aria-label="Sizing example input"
+//                                 aria-describedby={`title${item.id}`}
+//                                 value={item.title}
+//                                 disabled
+//                             />
+//                         </div>
+//                     </li>
+//                     <li className="list-group-item">
+//                         <div className="input-group mb-3">
+//                             <span className="input-group-text" id={`link${item.id}`}>
+//                                 Link:
+//                             </span>
+//                             <input
+//                                 type="text"
+//                                 className="form-control custom-link"
+//                                 id={`link${item.id}`}
+//                                 aria-label="Sizing example input"
+//                                 aria-describedby={`link${item.id}`}
+//                                 value={item.link}
+//                                 onClick={() => window.open(item.link, '_blank')}
+//                                 readOnly
+//                             />
+//                             <a href={item.link} target="_blank" rel="noopener noreferrer">
+//                                 <i className="fa-solid fa-globe"></i>
+//                             </a>
+//                         </div>
+//                     </li>
+//                     <li className="list-group-item">
+//                         <div className="input-group mb-3">
+//                             <label className="input-group-text" htmlFor={`status${item.id}`}>
+//                                 Estatus:
+//                             </label>
+//                             <select
+//                                 className="form-select"
+//                                 id={`status${item.id}`}
+//                                 value={item.status}
+//                                 disabled
+//                             >
+//                                 <option value=""></option>
+//                                 <option value="Disponible">Disponible</option>
+//                                 <option value="Reservado">Reservado</option>
+//                             </select>
+//                         </div>
+//                     </li>
+//                 </ul>
+//                 <div className="card-footer text-center">
+//                     <Link to={`/giftlist/${uid}/edit/${item.id}`}>
+//                         <button href="#" className="btn btn-primary">Editar</button>
+//                     </Link>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+// ));
+// };
+
--- src/front/js/component/renderGiftsPurchased.js
+++ src/front/js/component/renderGiftsPurchased.js
@@ -9,8 +9,67 @@ export const RenderGiftsPurchased = () => {
 
 
     return (
-        <div>
-            <h1> Todos los regalos reservados</h1>
+        <div className="col">
+            <div className="card">
+                <div className="top-icons-card d-flex justify-content-end p-2">
+                    <i className="fa-solid fa-circle-xmark"></i>
+                </div>
+                <div className="imgCard text-center">
+                </div>
+                <div className="card-body">
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item">
+                            <div className="input-group mb-3">
+                                <span className="input-group-text">
+                                    Título:
+                                </span>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    aria-label="Sizing example input"
+                                    disabled
+                                />
+                            </div>
+                        </li>
+                        <li className="list-group-item">
+                            <div className="input-group mb-3">
+                                <span className="input-group-text">
+                                    Link:
+                                </span>
+                                <input
+                                    type="text"
+                                    className="form-control custom-link"
+                                    aria-label="Sizing example input"
+                                    readOnly
+                                />
+                                {/* <a href={item.link} target="_blank" rel="noopener noreferrer">
+                                    <i className="fa-solid fa-globe"></i>
+                                </a> */}
+                            </div>
+                        </li>
+                        <li className="list-group-item">
+                            <div className="input-group mb-3">
+                                <label className="input-group-text">
+                                    Estatus:
+                                </label>
+                                <select
+                                    className="form-select"
+                                    disabled
+                                >
+                                    <option value=""></option>
+                                    <option value="Disponible">Disponible</option>
+                                    <option value="Reservado">Reservado</option>
+                                </select>
+                            </div>
+                        </li>
+                    </ul>
+                    <div className="card-footer text-center">
+                        <Link to="#">
+                            <button href="#" className="btn btn-primary">Editar</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
