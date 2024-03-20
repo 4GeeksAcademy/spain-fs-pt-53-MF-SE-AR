dccc04c9ac3a6c9784ea4e947c375e3ab4452cb6
Sabrina Estévez <sesmo.dev@gmail.com>
2024-03-20 12:20:34 +0000

    Home frontend DONE

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -15,7 +15,7 @@ export const Home = () => {
 	useEffect(() => {
 
 		if (sessionStorage.token && sessionStorage.token !== null && sessionStorage.token !== "") {
-			
+
 			setIsAuthenticated(true);
 			actions.getUserToStore();
 		} else {
@@ -26,8 +26,8 @@ export const Home = () => {
 
 	return (
 		<div className="container mt-5 ">
-			<div className="d-flex justify-content-around">
-				<div className="">
+			<div className="home-welcome row">
+				<div className="col">
 					<h1>
 						Welcome to GiftBuddy!
 					</h1>
@@ -36,24 +36,24 @@ export const Home = () => {
 						<button className="btn">Start now</button>
 					</Link>
 				</div>
-				<div className="d-inline">
+				<div className="col">
 					<img src={homeGift} className="img-drawing " />
 				</div>
 			</div>
-			<h2>
-				How does it work?
-			</h2>
-			<div className="d-flex justify-content-around" >
-				<div className="d-inline">
-					<p>Make a list of the presents that you or your beloved would like to receive.</p>
+			<div className="row home-steps" >
+				<h2>
+					How does it work?
+				</h2>
+				<div className="box-steps col">
+					<p> 1 <i className="fa-solid fa-arrow-right" ></i> Make a list of the presents that you or your beloved would like to receive.</p>
 					<img src={howToGift1} className="img-drawing " />
 				</div>
-				<div className="d-inline">
-					<p>Send the list to the party guests so they can choose the one they want to give.</p>
+				<div className="box-steps col">
+					<p> 2 <i className="fa-solid fa-arrow-right" ></i> Send the list to the party guests so they can choose the one they want to give.</p>
 					<img src={howToGift2} className="img-drawing " />
 				</div>
-				<div className="d-inline">
-					<p>You won't know who picked it up, just that someone chose it.</p>
+				<div className="box-steps col">
+					<p> 3 <i className="fa-solid fa-arrow-right" ></i> You won't know who picked it up, just that someone chose it.</p>
 					<img src={howToGift3} className="img-drawing " />
 				</div>
 			</div>
